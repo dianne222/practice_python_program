81cd1165b149fb64bd3cf4ccedfe9f6dba501acc
Dianne <crisdiannecasino2@gmail.com>
2026-03-11 05:11:52 +0800

    created the python program that multiply the two numbers

---
+++ batch_1_number_codes/product_of_two_numbers.py
@@ -0,0 +1,10 @@
+# Prog04: Create a program that ask user to input 2 numbers.
+# Print the product of the two numbers.
+
+# Input 2 numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+# multiply the two numbers and then print the product
+product = first_number * second_number
+print("The product is:", product)
