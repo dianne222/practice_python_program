e8ef3fc7a78054edfcb4e0edbb1f0818f3aae7af
Dianne <crisdiannecasino2@gmail.com>
2026-03-11 04:19:26 +0800

    python program that prints name in proper casing

---
+++ name_in_proper_casing.py
@@ -0,0 +1,8 @@
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing.
+# Print the input in proper casing.
+
+# Input full name
+full_name = input("Enter your full name(in incorrect casing): ")
+
+# Print the name in proper casing
+print(full_name.title())
