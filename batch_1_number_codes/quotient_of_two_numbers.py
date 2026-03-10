53a48a20d46e114b629697d9d24f74353b76f7d7
Dianne <crisdiannecasino2@gmail.com>
2026-03-11 05:14:30 +0800

    created the code that print the quotient of two numbers

---
+++ batch_1_number_codes/quotient_of_two_numbers.py
@@ -0,0 +1,10 @@
+# Prog05: Create a program that ask user to input 2 numbers.
+# Print the quotient of the two numbers with the decimal point
+
+# Input 2 numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+# divide the two numbers and then print the quotient
+quotient = first_number / second_number
+print("The quotient is:", quotient)
