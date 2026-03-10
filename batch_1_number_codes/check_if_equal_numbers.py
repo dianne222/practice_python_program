f81d46996a95e57276863d6e02bdb97a80b69dca
Dianne <crisdiannecasino2@gmail.com>
2026-03-11 05:05:43 +0800

    python program that checks if the numbers are equal

---
+++ batch_1_number_codes/check_if_equal_numbers.py
@@ -0,0 +1,12 @@
+# Prog02: Create a program that ask user to input 2 numbers.
+# Print "Equal" when the numbers are the same.
+
+# Input 2 numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+# Print "Equal" when two numbers are same
+if first_number == second_number:
+    print("Equal")
+else:
+    print("Not Equal")
