8a7da0952de0c50aa775866f871ce7b58cd6c215
Dianne <crisdiannecasino2@gmail.com>
2026-03-11 20:54:05 +0800

    updated the program that counts odd numbers, using while loop

--- batch_2_number_codes/display_odd_numbers.py
+++ batch_2_number_codes/display_odd_numbers.py
@@ -1,6 +1,10 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
-# Loop to check odd numbers from 0-100, then print the odd numbers
-for i in range(101):
-    if i % 2 != 0:
-        print(i, end = " ")
+# Variable for number that is being checked if odd or not
+number = 0
+
+# Checks the numbers 0-100 if odd or not, if yes print it. Using while loop
+while number <= 100:
+    if number % 2 != 0:
+        print(number, end=' ')
+    number += 1
