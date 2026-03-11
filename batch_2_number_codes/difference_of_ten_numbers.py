8121303189abf883fe185dfd2c60fa6cd0d745c0
Dianne <crisdiannecasino2@gmail.com>
2026-03-11 20:33:43 +0800

    created a program that subtract the other nine numbers to the first number

---
+++ batch_2_number_codes/difference_of_ten_numbers.py
@@ -0,0 +1,17 @@
+# Prog06: Create a program that ask user to input 10 numbers.
+# Print the result of the first number minus all of the remaining numbers.
+
+# Variable where first number is added then the remaining number is subtracted
+difference_of_ten_numbers = 0
+
+# Input first number to be added to the variable
+number = float(input("Enter a number: "))
+difference_of_ten_numbers += number
+
+# Input numbers to be subtracted to the variable, repeat 9 times
+for i in range(9):
+    number = float(input("Enter a number: "))
+    difference_of_ten_numbers -= number
+
+# Print the result
+print(difference_of_ten_numbers)
