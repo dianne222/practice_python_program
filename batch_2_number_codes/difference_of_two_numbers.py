4be117ae611bbddd991b2b00bf8cb18cdbbd5b7c
Dianne <crisdiannecasino2@gmail.com>
2026-03-11 18:25:21 +0800

    created program that subtract two numbers

---
+++ batch_2_number_codes/difference_of_two_numbers.py
@@ -0,0 +1,9 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+# Input 2 numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+# Subtract the two numbers and then print the difference
+difference = first_number - second_number
+print("The difference is:", difference)
