d69799c9186f063bcda5f0c88c11534b7340b32d
Dianne <crisdiannecasino2@gmail.com>
2026-03-11 17:44:46 +0800

    created the program that adds ten numbers

---
+++ batch_1_number_codes/sum_of_ten_numbers.py
@@ -0,0 +1,13 @@
+# Prog07: Create a program that ask user to input 10 numbers.
+# Print the sum of all the numbers.
+
+# variable where the ten numbers is added
+sum_of_ten_numbers = 0
+
+# Input number and add it to the variable, loop 10 times
+for i in range(10):
+    number = float(input("Enter a number: "))
+    sum_of_ten_numbers += number
+
+# Print the sum of 10 numbers
+print(sum_of_ten_numbers)
