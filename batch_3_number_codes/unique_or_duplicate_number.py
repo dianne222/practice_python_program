23734203e71ce0e38a5dc8ce628389b735801341
Dianne <crisdiannecasino2@gmail.com>
2026-03-13 01:08:07 +0800

    added a list for the numbers and append the input to the list

--- batch_3_number_codes/unique_or_duplicate_number.py
+++ batch_3_number_codes/unique_or_duplicate_number.py
@@ -1,7 +1,12 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid.
 # Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
-# Input a number with unlimited loop until invalid input
+# List for the numbers input
+numbers = []
+
+# Input a number with unlimited loop until invalid input.
 for i in range():
     number = int(input("Enter a number: "))
+    numbers.append(number)
+
     
