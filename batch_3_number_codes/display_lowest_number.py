e6c13e16293a04f85bdea6a944d4d993d07ede06
Dianne <crisdiannecasino2@gmail.com>
2026-03-13 01:22:26 +0800

    added input function that runs until invalid input

---
+++ batch_3_number_codes/display_lowest_number.py
@@ -0,0 +1,10 @@
+# Prog04: Create a program that ask user to input a number,
+# continue asking until the user input is invalid. Display the lowest number
+
+# Input number until invalid
+while True:
+    try:
+        number = int(input("Enter a number: "))
+
+    except ValueError:
+        break
