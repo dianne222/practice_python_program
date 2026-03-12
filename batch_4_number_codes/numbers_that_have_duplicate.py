da0bc9097355331a2a17104596b5735a60823636
Dianne <crisdiannecasino2@gmail.com>
2026-03-13 01:50:19 +0800

    added print list for numbers and print function

--- batch_4_number_codes/numbers_that_have_duplicate.py
+++ batch_4_number_codes/numbers_that_have_duplicate.py
@@ -1,5 +1,15 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
+# List for the numbers
+numbers = []
+
 # Input 10 numbers
 for i in range(10):
     number = int(input("Enter a number: "))
+    numbers.append(number)
+
+# Check if the number have duplicate then print it
+for number in numbers:
+    if numbers.count(number) > 1:
+        print("Numbers that have duplicate:")
+        print(number, end = " ")
