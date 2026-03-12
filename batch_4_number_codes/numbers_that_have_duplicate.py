7f45a999bdc126c4179ec92d982fe121a761f2d0
Dianne <crisdiannecasino2@gmail.com>
2026-03-13 01:42:35 +0800

    added input function

---
+++ batch_4_number_codes/numbers_that_have_duplicate.py
@@ -0,0 +1,5 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+# Input 10 numbers
+for i in range(10):
+    number = int(input("Enter a number: "))
