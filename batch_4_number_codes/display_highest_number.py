d4e73217541ad3e0829b5bb7210bc160ae7564e9
Dianne <crisdiannecasino2@gmail.com>
2026-03-18 21:24:11 +0800

    added a list for the numbers

--- batch_4_number_codes/display_highest_number.py
+++ batch_4_number_codes/display_highest_number.py
@@ -1,10 +1,14 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid.
 # Display the highest number
 
+# List for the numbers
+number_list = []
+
 # Input number until invalid
 while True:
     try:
         number = int(input("Enter a number: "))
+        number_list.append(number)
 
     except ValueError:
         break
