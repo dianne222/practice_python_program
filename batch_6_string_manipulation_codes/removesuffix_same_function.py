facd59f1274ef7008a34efeb82b86e977e707072
Dianne <crisdiannecasino2@gmail.com>
2026-03-18 23:27:45 +0800

    added a list of files

---
+++ batch_6_string_manipulation_codes/removesuffix_same_function.py
@@ -0,0 +1,5 @@
+# Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter.
+# Create a program that do the same functionality without using removesuffix() function.
+
+# List of files
+files = ["python.txt", "java.txt", "c++.txt", "html"]
