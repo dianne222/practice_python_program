77a591bf139fbcbd9b5ee3852b40f1c50887d5a5
Dianne <crisdiannecasino2@gmail.com>
2026-03-18 21:32:50 +0800

    added a list for the numbers and sort it from highest to lowest

--- batch_4_number_codes/number_highest_to_lowest.py
+++ batch_4_number_codes/number_highest_to_lowest.py
@@ -1,10 +1,15 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid.
 # Display the number from highest to lowest. Clue: sort() function
 
+# List for the numbers
+number_list = []
+
 # Input a number until invalid
 while True:
     try:
         number = int(input("Enter a number: "))
+        number_list.append(number)
 
     except ValueError:
+        number_list.sort(reverse=True)
         break
