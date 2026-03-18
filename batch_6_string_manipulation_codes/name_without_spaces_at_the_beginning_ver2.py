525278eda5827e4664f55b99cb9f4350836989ee
Dianne <crisdiannecasino2@gmail.com>
2026-03-18 22:11:37 +0800

    added a function where it put the words in string to the list then join the words together with spaces in between

--- batch_6_string_manipulation_codes/name_without_spaces_at_the_beginning_ver2.py
+++ batch_6_string_manipulation_codes/name_without_spaces_at_the_beginning_ver2.py
@@ -4,4 +4,9 @@
 # Input name with spaces at the beginning
 full_name = input("Enter your full name(with spaces at the beginning): ")
 
+# Remove spaces at the beginning
+word_list = full_name.split()
+full_name = " ".join(word_list)
+
+# Print the name
 print(full_name)
