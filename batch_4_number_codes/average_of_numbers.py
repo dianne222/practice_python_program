99614958e86c901862d09b9f9d3c135501f3b2b2
Dianne <crisdiannecasino2@gmail.com>
2026-03-18 21:40:48 +0800

    added a list for numbers and a code that add all the numbers in list then divide it by the length of the list, then display it which is the average

--- batch_4_number_codes/average_of_numbers.py
+++ batch_4_number_codes/average_of_numbers.py
@@ -1,10 +1,16 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid.
 # Display the average.
 
+# List for the numbers
+number_list = []
+
 # Input number until invalid
 while True:
     try:
         number = int(input("Enter a number: "))
+        number_list.append(number)
 
     except ValueError:
+        average = sum(number_list) / len(number_list)
+        print("Average: ", average)
         break
