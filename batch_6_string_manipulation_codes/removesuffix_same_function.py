021496f2a148de9c3cf48f65f21125be28033000
Dianne <crisdiannecasino2@gmail.com>
2026-03-18 23:29:43 +0800

    added a code that can replace removesuffix function then print the result

--- batch_6_string_manipulation_codes/removesuffix_same_function.py
+++ batch_6_string_manipulation_codes/removesuffix_same_function.py
@@ -3,3 +3,14 @@
 
 # List of files
 files = ["python.txt", "java.txt", "c++.txt", "html"]
+file_name = []
+
+# Code that can replace removesuffix() function
+for file in files:
+    if file.endswith(".txt"):
+        file_name.append(file.replace(".txt", ""))
+    else:
+        file_name.append(file)
+
+# Print the result
+print(str(file_name)[1:-1])
