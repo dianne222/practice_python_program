d7e799f54829d8f72d80704e08027c44b3b67024
Dianne <crisdiannecasino2@gmail.com>
2026-03-18 20:47:41 +0800

    added a dictionary to count the duplicates each number

--- batch_4_number_codes/number_with_most_duplicate.py
+++ batch_4_number_codes/number_with_most_duplicate.py
@@ -1,10 +1,21 @@
 # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid.
 # Display the number with the most number of duplicate.
 
+# List for the numbers
+numbers = []
+duplicates = {}
+most_duplicate = 0
+
+
 # Input number until invalid
 while True:
     try:
         number = int(input("Enter a number: "))
-        
+        numbers.append(number)
+
     except ValueError:
+        numbers.sort()
+        for i in numbers:
+            duplicates[i] = numbers.count(i)
+
         break
