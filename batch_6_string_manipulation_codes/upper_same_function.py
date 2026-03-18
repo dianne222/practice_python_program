dd145e151f289d0ed29f6356619d85f4387246a0
Dianne <crisdiannecasino2@gmail.com>
2026-03-18 23:33:02 +0800

    added an input function and print

---
+++ batch_6_string_manipulation_codes/upper_same_function.py
@@ -0,0 +1,7 @@
+# Prog03. upper() converts all characters of the string into upper case.
+# Create a program that do the same functionality without using upper() function.
+
+# Input full name in lower case
+full_name = input("Enter your full name(in lower case): ")
+
+print(full_name)
