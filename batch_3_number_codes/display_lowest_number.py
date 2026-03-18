5423617b693c9b762e405f5e3077585d6e16152f
Dianne <crisdiannecasino2@gmail.com>
2026-03-18 21:27:50 +0800

    changed the code to more simple

--- batch_3_number_codes/display_lowest_number.py
+++ batch_3_number_codes/display_lowest_number.py
@@ -1,21 +1,15 @@
 # Prog04: Create a program that ask user to input a number,
 # continue asking until the user input is invalid. Display the lowest number
 
-# variable for lowest number and the count of input
-lowest_number = 0
-count = 0
+# list for the number
+number_list = []
+
 # Input number until invalid.
 while True:
     try:
         number = int(input("Enter a number: "))
-        # Make the first input the lowest number then compare the rest if it is lower than the lowest
-        if count == 0:
-            lowest_number = number
-        else:
-            if number < lowest_number:
-                lowest_number = number
-        count += 1
+        number_list.append(number)
 
     except ValueError:
-        print("Lowest number:", lowest_number)
+        print("Lowest number:", min(number_list))
         break
