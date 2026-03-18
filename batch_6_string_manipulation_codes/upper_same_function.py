0b2c31704a6c00d809684b266a879a0d7c92fd0e
Dianne <crisdiannecasino2@gmail.com>
2026-03-18 23:41:01 +0800

    added a function that checks if it is lowercase or upper case then convert the lower to upper then put it into a list

--- batch_6_string_manipulation_codes/upper_same_function.py
+++ batch_6_string_manipulation_codes/upper_same_function.py
@@ -1,7 +1,20 @@
 # Prog03. upper() converts all characters of the string into upper case.
 # Create a program that do the same functionality without using upper() function.
 
+# list for characters
+characters = []
+
 # Input full name in lower case
 full_name = input("Enter your full name(in lower case): ")
+
+# turns the characters into upper case
+for i in full_name:
+    if 'a' <= i <= 'z':
+        characters.append(ord(i) - 32) #converts the lower case letter to upper case using ASCII
+    elif 'A' <= i <= 'Z':
+        characters.append(i)
+
+
+
 
 print(full_name)
