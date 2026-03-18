fd09e7874990246a152926a309b6a90336b601ce
Dianne <crisdiannecasino2@gmail.com>
2026-03-18 22:03:26 +0800

    added a input and print function for name string

---
+++ batch_6_string_manipulation_codes/name_without_spaces_in_the_beginning_ver2.py
@@ -0,0 +1,7 @@
+# Prog01. lstrip() remove the space characters at the beginning of the string.
+# Create a program that do the same functionality without using lstrip() function.
+
+# Input name with spaces at the beginning
+full_name = input("Enter your full name(with spaces at the beginning): ")
+
+print(full_name)
